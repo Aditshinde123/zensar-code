02b977e22a0a4017b9eaad35fd20e90eaa17c19f
Aditshinde123 <91326751+Aditshinde123@users.noreply.github.com>
2022-04-18 18:19:13 +0530

    Add files via upload

---
+++ agrsPackage/class_file.py
@@ -0,0 +1,15 @@
+class StartStopwitharguement:
+
+    def __init__(self,name4,name5,name6):
+
+        self.name4 = name4
+
+        self.name5 = name5
+
+        self.name6 = name6
+
+    def __init__(self,function):
+        self.function = function
+
+    def call(self,*args):
+        self.function(*args)
